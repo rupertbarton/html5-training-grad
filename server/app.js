19d89e67400582fca4fe0392024f1c280099f1db
Rupert Barton <rbarton@scottlogic.com>
2019-03-05 17:14:58 +0000

    basic webscokets work, start work on main websocket applications

--- server/app.js
+++ server/app.js
@@ -2,20 +2,31 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var routes = require("./app/routes.js");
 var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+
+server.listen(3001, function () {
+  console.log("app running on port", server.address().port);
+});
+
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', "http://localhost:3000");
-    res.header('Access-Control-Allow-Methods', 'GET,POST');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    next();
+  res.header('Access-Control-Allow-Origin', "http://localhost:3000");
+  res.header('Access-Control-Allow-Methods', 'GET,POST');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
+routes(app);
+
+
+io.on('connection', function (socket) {
+  console.log("made connection")
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
   });
-
-  routes(app);
-
-
-var server = app.listen(3001, function () {
-    console.log("app running on port", server.address().port);
-}); 
+})
--- server/app/routes.js
+++ server/app/routes.js
@@ -4,11 +4,11 @@ var appRouter = function (app) {
 
   const mainMatcher = new Matcher()
 
-  let c = new Order("a",5,5,"sell")
-  let d = new Order("a",1000,5,"sell")
-  
-  let e = new Order("a",4.5,5,"buy")
-  let f = new Order("a",1,5,"buy")
+  let c = new Order("a", 5, 5, "sell")
+  let d = new Order("a", 1000, 5, "sell")
+
+  let e = new Order("a", 4.5, 5, "buy")
+  let f = new Order("a", 1, 5, "buy")
   mainMatcher.matcher(c)
   mainMatcher.matcher(c)
   mainMatcher.matcher(c)
@@ -19,7 +19,7 @@ var appRouter = function (app) {
   mainMatcher.matcher(f)
 
 
-    app.get("/", function (req, res) {       //Load all orders and trade history
+  app.get("/", function (req, res) {       //Load all orders and trade history
     aggregate = Number(req.query.aggregate) || 0.01;
     res.status(200).send([mainMatcher.orderBook.createAgreggatedOrderBook(0.01), mainMatcher.tradeHistory]);
   });
@@ -29,17 +29,22 @@ var appRouter = function (app) {
     console.log(mainMatcher.orderBook.getAccountOrders(name))
     res.status(200).send(mainMatcher.orderBook.getAccountOrders(name));
   });
-  
+
   app.post("/newOrder", function (req, res) {      //add new order
     name = req.body.account;
     quantity = Number(req.body.quantity);
     price = Number(req.body.price);
     action = req.body.action;
     aggregate = Number(req.body.aggregate) || 0.01;
-    
+
     newOrder = new Order(name, price, quantity, action);
     mainMatcher.matcher(newOrder);
     res.status(201).send([mainMatcher.orderBook.createAgreggatedOrderBook(aggregate), mainMatcher.tradeHistory, mainMatcher.orderBook.getAccountOrders(name),]);
+
+    io.Socket.emit("agredgatedOrderBook", aggregatedOrderBooked);
+    io.Socket.emit("agredgatedOrderBook", 
+    aggregatedOrderBooked);
+    io.Socket.emit("agredgatedOrderBook", aggregatedOrderBooked);
   });
 }
 
